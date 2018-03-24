4f67c58c4c06be83c23d50fe6b76719a48fd09c1
Acharya <arup.acharya@bt.com>
2018-03-24 16:06:48 +0530

    Final

--- src/mobileAutomation/Ebay.java
+++ src/mobileAutomation/Ebay.java
@@ -9,19 +9,15 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
-
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
-
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.TakesScreenshot;
-
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -42,11 +38,11 @@ public class Ebay {
 	DateFormat df;
 	ExtentReports extent;
 	ExtentTest logger;
-	
+
 	protected AndroidDriver<AndroidElement> driver = null;
 
 	DesiredCapabilities dc = new DesiredCapabilities();
-	
+
 	@BeforeMethod
 	public void setUp() throws MalformedURLException {
 		dc.setCapability("reportDirectory", reportDirectory);
@@ -57,7 +53,7 @@ public class Ebay {
 		dc.setCapability(MobileCapabilityType.UDID, "ZY22232ZB2");
 		driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), dc);
 	}
-	
+
 	@BeforeTest
 	public void startTest(){
 		extent = new ExtentReports(System.getProperty("user.dir")+"/test-output/MobileAutomationReport.html",true);
@@ -67,13 +63,13 @@ public class Ebay {
 
 		extent.loadConfig(new File(System.getProperty("user.dir")+"\\extent-config.xml"));
 	}
-	
+
 	@Test
 	public void BuyPhone() throws Exception {
-		
-		
+
+
 		File file = new File(System.getProperty("user.dir")+"\\src\\com\\config\\OR.properties");
-		  
+
 		FileInputStream fileInput = null;
 		try {
 			fileInput = new FileInputStream(file);
@@ -81,14 +77,14 @@ public class Ebay {
 			e.printStackTrace();
 		}
 		Properties prop = new Properties();
-		
+
 		//load properties file
 		try {
 			prop.load(fileInput);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-				
+
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 		driver.context(prop.getProperty("AppType"));
@@ -103,25 +99,25 @@ public class Ebay {
 		captureScreenShots();
 		//Handling Mobile Screen Rotation. Changing it to landscape
 		driver.rotate(ScreenOrientation.LANDSCAPE);
-		
+
 		new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath(prop.getProperty("username"))));
 		driver.findElement(By.xpath(prop.getProperty("username"))).clear();
 		driver.findElement(By.xpath(prop.getProperty("username"))).sendKeys(prop.getProperty("usernameValue"));
-		
+
 		new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath(prop.getProperty("password"))));
 		driver.findElement(By.xpath(prop.getProperty("password"))).sendKeys(prop.getProperty("passwordValue"));
-		
+
 		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath(prop.getProperty("SignInButton2"))));
 		driver.findElement(By.xpath(prop.getProperty("SignInButton2"))).click();
 		captureScreenShots();
-		
+
 		new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath(prop.getProperty("NoThanks"))));
 		driver.findElement(By.xpath(prop.getProperty("NoThanks"))).click();
 		captureScreenShots();
-		
+
 		//Handling Mobile Screen Rotation. Changing it to Portrait again
 		driver.rotate(ScreenOrientation.PORTRAIT);
-		
+
 		new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath(prop.getProperty("SearchBox"))));
 		driver.findElement(By.xpath(prop.getProperty("SearchBox"))).click();
 
@@ -133,12 +129,12 @@ public class Ebay {
 		new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath(prop.getProperty("PhoneName"))));
 		driver.findElement(By.xpath(prop.getProperty("PhoneName"))).click();
 		captureScreenShots();
-		
+
 		new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath(prop.getProperty("BuyNowButton"))));
 		driver.findElement(By.xpath(prop.getProperty("BuyNowButton"))).click();
 		captureScreenShots();
 		//Till buy it now screen
-		
+
 		logger = extent.startTest("BuyPhone");
 		Assert.assertTrue(true);
 		logger.log(LogStatus.PASS, "Test Case passed");
@@ -149,23 +145,23 @@ public class Ebay {
 	public void tearDown() {
 		driver.quit();
 	}
-	
+
 	public void captureScreenShots() throws IOException {
-        folder_name="screenshot";
-        File f=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        //Date format for screenshot file name
-        df=new  SimpleDateFormat("dd-MMM-yyyy__hh_mm_ssaa");
-        //create directory with given folder name
-        new File(folder_name).mkdir();
-        //Setting file name
-        String file_name=df.format(new Date())+".png";
-        //copy screenshot file into screenshot folder.
-        FileUtils.copyFile(f, new File(folder_name + "/" + file_name));
-    }
-	
+		folder_name="screenshot";
+		File f=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		//Date format for screenshot file name
+		df=new  SimpleDateFormat("dd-MMM-yyyy__hh_mm_ssaa");
+		//create directory with given folder name
+		new File(folder_name).mkdir();
+		//Setting file name
+		String file_name=df.format(new Date())+".png";
+		//copy screenshot file into screenshot folder.
+		FileUtils.copyFile(f, new File(folder_name + "/" + file_name));
+	}
+
 	@AfterTest
-	 public void endReport(){
-	                extent.flush();
-	                extent.close();
-	    }
+	public void endReport(){
+		extent.flush();
+		extent.close();
+	}
 }
